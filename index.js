70bf28e822ffa9bfeedea79f015e496c50924165
Grish8 <grishamenos@gmail.com>
2025-02-14 20:28:21 +0300

    made the websit responsive to devices

--- index.js
+++ index.js
@@ -12,16 +12,22 @@ function submitRating() {
         localStorage.setItem("userRating", rating.value);
         alert("Thank you for rating: " + rating.value + " stars!");
         closeModal();
-        window.location.href = "ratings.html"; // Redirect to ratings page
+        window.location.href = ""; // Redirect to ratings page
     } else {
         alert("Please select a star rating before submitting.");
     }
 }
 
-// Close modal when clicking outside of it
-window.onclick = function(event) {
-    let modal = document.getElementById("ratingModal");
-    if (event.target === modal) {
-        modal.style.display = "none";
-    }
+
+
+
+// Function to Toggle Menu Visibility
+function toggleMenu() {
+    var nav = document.getElementById("nav-menu");
+    nav.classList.toggle("active"); // Adds or removes 'active' class
+}
+
+// Function to Close Menu
+function closeMenu() {
+    document.getElementById("nav-menu").classList.remove("active"); // Hides menu
 }
